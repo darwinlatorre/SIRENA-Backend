5a5f18b3fd35d223de9edca67b8e3c66a1bb37d5
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2023-09-14 01:46:59 -0500

    feat: arquitectura del proyecto
    Añadido a la aquitectura del proyecto se definen los modelos principales (users, rols).

---
+++ src/main/java/co/edu/unicauca/SIRENABackend/models/RoleModel.java
@@ -0,0 +1,30 @@
+package co.edu.unicauca.SIRENABackend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class RoleModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", unique = true)
+    private Integer roleID;
+
+    @Column(name = "role_name", nullable = false, length = 20)
+    private String roleName;
+    
+}
---
+++ src/main/java/co/edu/unicauca/SIRENABackend/services/RoleService.java
@@ -0,0 +1,8 @@
+package co.edu.unicauca.SIRENABackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+    
+}
