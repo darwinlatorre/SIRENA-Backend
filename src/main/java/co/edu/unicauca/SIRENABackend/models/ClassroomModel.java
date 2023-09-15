3890cc59ad35ea12dbeebef0be5c8d93862b1862
Andrés Felipe Ocampo <89326187+AndresFelipeO@users.noreply.github.com>
2023-09-14 22:51:48 -0500

    Implementación notación del classroom

--- src/main/java/co/edu/unicauca/SIRENABackend/models/ClassroomModel.java
+++ src/main/java/co/edu/unicauca/SIRENABackend/models/ClassroomModel.java
@@ -24,9 +24,19 @@ public class ClassroomModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cls_id", unique = true)
     private  Integer clsId;
+
+    @Column(name = "cls_name", nullable = false, length = 20)
     private  String clsName;
+
+    @Column(name = "cls_capacity", nullable = false, length = 20)
     private Integer clsCapacity;
+
+    @Column(name = "cls_state", nullable = false, length = 20)
     private  String clsState;
+
+    @Column(name = "cls_building", nullable = false, length = 20)
     private  String clsBuilding;
+
+    @Column(name = "cls_type", nullable = false, length = 20)
     private  String clsType;
 }
