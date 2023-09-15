38c215ebc6334a79bf262506edc05f841396bf5c
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2023-09-15 02:13:12 -0500

    feat: CRUD user

--- src/main/java/co/edu/unicauca/SIRENABackend/services/UserService.java
+++ src/main/java/co/edu/unicauca/SIRENABackend/services/UserService.java
@@ -1,8 +1,39 @@
 package co.edu.unicauca.SIRENABackend.services;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.unicauca.SIRENABackend.models.UserModel;
+import co.edu.unicauca.SIRENABackend.repositories.IUserRepository;
 
 @Service
 public class UserService {
     
+    @Autowired
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers(){
+        return (ArrayList<UserModel>)userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel prmUser){
+        return userRepository.save(prmUser);
+    }
+
+    public Optional<UserModel> getUserById(Integer prmId){
+        return userRepository.findById(prmId);
+    }
+
+    public boolean deleteUserById(Integer prmId){
+        try {
+            userRepository.deleteById(prmId);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
