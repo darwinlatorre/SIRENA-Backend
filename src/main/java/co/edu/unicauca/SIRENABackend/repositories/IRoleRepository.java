dc401fe3da0ae870a7a64fd31c997a0bbbfaef9c
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2023-09-15 02:12:26 -0500

    fix: name

--- src/main/java/co/edu/unicauca/SIRENABackend/repositories/IRoleRepository.java
+++ src/main/java/co/edu/unicauca/SIRENABackend/repositories/IRoleRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import co.edu.unicauca.SIRENABackend.models.RoleModel;
 
 public interface IRoleRepository extends JpaRepository<RoleModel, Integer>{
-    
 }
