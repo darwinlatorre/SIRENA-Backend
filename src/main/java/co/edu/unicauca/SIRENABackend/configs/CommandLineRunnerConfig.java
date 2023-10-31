3d03e435664c5ee534e6eee2814002a43866fdc7
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2023-10-30 20:27:10 -0500

    clean: comments

--- src/main/java/co/edu/unicauca/SIRENABackend/configs/CommandLineRunnerConfig.java
+++ src/main/java/co/edu/unicauca/SIRENABackend/configs/CommandLineRunnerConfig.java
@@ -36,53 +36,50 @@ public class CommandLineRunnerConfig {
     }
 
     private void registroUsuarios(AuthService authService) {
-        var adminUser = UserRegisterReq.builder()
-                .usr_name("admin")
-                .usr_firstname("admin")
-                .usr_lastname("admin")
-                .usr_email("admin@unicauca.edu.co")
-                .usr_password("admin")
-                .usr_role(ADMIN)
-                .build();
 
         try {
-            System.out.println("Admin token: " + authService.register(adminUser).getAccesToken());
+            var adminUser = UserRegisterReq.builder()
+                    .usr_name("admin")
+                    .usr_firstname("admin")
+                    .usr_lastname("admin")
+                    .usr_email("admin@unicauca.edu.co")
+                    .usr_password("admin")
+                    .usr_role(ADMIN)
+                    .build();
+            authService.register(adminUser);
+            System.out.println("El usuario admin ha sido registrado");
         } catch (Exception e) {
-            // System.out.println("Error al registrar el usuario admin: " + e.getMessage());
             System.out.println("El usuario admin ya exsite");
         }
 
-        var coordinadorTestUser = UserRegisterReq.builder()
-                .usr_name("coordinadorTest")
-                .usr_firstname("coordinadorTest")
-                .usr_lastname("coordinadorTest")
-                .usr_email("coordinadorTest@unicauca.edu.co")
-                .usr_password("coordinadorTest")
-                .usr_role(COORDINADOR)
-                .build();
-
         try {
-            System.out.println("Coordinador token: " + authService.register(coordinadorTestUser).getAccesToken());
+            var coordinadorTestUser = UserRegisterReq.builder()
+                    .usr_name("coordinadorTest")
+                    .usr_firstname("coordinadorTest")
+                    .usr_lastname("coordinadorTest")
+                    .usr_email("coordinadorTest@unicauca.edu.co")
+                    .usr_password("coordinadorTest")
+                    .usr_role(COORDINADOR)
+                    .build();
+            authService.register(coordinadorTestUser);
+            System.out.println("El usuario coordinadorTest ha sido registrado");
         } catch (Exception e) {
-            // System.out.println("Error al registrar el usuario coordinadorTest: " +
-            // e.getMessage());
             System.out.println("El usuario coordinadorTest ya existe");
         }
 
-        var docenteTestUser = UserRegisterReq.builder()
-                .usr_name("docenteTest")
-                .usr_firstname("docenteTest")
-                .usr_lastname("docenteTest")
-                .usr_email("docenteTest@unicauca.edu.co")
-                .usr_password("docenteTest")
-                .usr_role(DOCENTE)
-                .build();
-
         try {
-            System.out.println("Docente token: " + authService.register(docenteTestUser).getAccesToken());
+            var docenteTestUser = UserRegisterReq.builder()
+                    .usr_name("docenteTest")
+                    .usr_firstname("docenteTest")
+                    .usr_lastname("docenteTest")
+                    .usr_email("docenteTest@unicauca.edu.co")
+                    .usr_password("docenteTest")
+                    .usr_role(DOCENTE)
+                    .build();
+
+            authService.register(docenteTestUser);
+            System.out.println("El usuario docenteTest ha sido registrado");
         } catch (Exception e) {
-            // System.out.println("Error al registrar el usuario docenteTest: " +
-            // e.getMessage());
             System.out.println("El usuario docenteTest ya existe");
         }
     }
