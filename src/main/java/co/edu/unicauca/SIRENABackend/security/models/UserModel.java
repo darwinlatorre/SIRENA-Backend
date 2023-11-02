fc0757f12e7ba8fa7153e39c4270e274ad84b249
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2023-11-02 18:33:26 -0500

    Remove unnecessary getUsername method in UserModel.java

--- src/main/java/co/edu/unicauca/SIRENABackend/security/models/UserModel.java
+++ src/main/java/co/edu/unicauca/SIRENABackend/security/models/UserModel.java
@@ -75,11 +75,6 @@ public class UserModel implements UserDetails {
     }
 
     @Override
-    public String getUsername() {
-        return "ID: " + id + ", " + username;
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
